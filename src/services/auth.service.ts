eba95702fb9dffc7eb3dd42d489882b982bbb060
Fabio Bione <fabiolopesbione@hotmail.com>
2020-01-17 22:28:25 -0300

    Terminando as funcionalidades do carrinho

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,13 +5,17 @@ import { API_CONFIG } from "../config/api.config";
 import { StorageService } from "./storage.services";
 import { LocalUser } from "../models/local_user";
 import { JwtHelper } from "angular2-jwt"
+import { CartService } from "./domain/cart.service";
 
 @Injectable()
 export class AuthService {
     
     jwtHelper: JwtHelper = new JwtHelper();
 
-    constructor(public http: HttpClient, public storage: StorageService){}
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public cartService: CartService){}
 
     authenticate(creds: CredenciaisDTO){
         return this.http.post(`${API_CONFIG.baseUrl}/login`,
@@ -38,6 +42,7 @@ export class AuthService {
             email: this.jwtHelper.decodeToken(tok).sub
         }
         this.storage.setLocalUser(user);
+        this.cartService.createOrClearCart();
     }
 
     logout() {
